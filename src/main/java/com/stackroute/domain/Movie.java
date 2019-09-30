eec8845a35e0ca1b9f6784f7bba8ec122ba97dd8
Chinmay Kalegaonkar <kalegaonkar.chinmay1@gmail.com>
2019-09-30 16:44:43 +0530

    aware-interface changes

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,7 +1,18 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
   Actor actor;
+  ApplicationContext context = null;
+  private BeanFactory beanFactory;
+  private String beanName;
+
   public Actor getActor() {
     return actor;
   }
@@ -10,9 +21,30 @@ public class Movie {
     this.actor = actor;
   }
 
+  public ApplicationContext getContext() {
+    return context;
+  }
+
+  public void setContext(ApplicationContext context) {
+    this.context = context;
+  }
+
   public void printActor(){
     System.out.println("actor details "+actor.getName()+" "+actor.getGender()+" "+actor.getAge());
   }
 
+  @Override
+  public void setBeanName(String name) {
+    this.beanName = name;
+  }
 
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.context = context;
+  }
+
+  @Override
+  public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+    this.beanFactory = beanFactory;
+  }
 }
